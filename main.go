9c54e7143e3a6be2fad7d2ae4366547195c38aa4
Niklas Rueber <nrueber@igus.net>
2024-09-30 18:11:28 +0200

    refactor: draw now uses loops with the board dimensions
    Co-authored-by: Stefan Boos <kontakt@boos.systems>
    Co-authored-by: Niklas Rueber <niklas@rueber.cloud>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>

--- main.go
+++ main.go
@@ -7,7 +7,22 @@ import (
 )
 
 type Game struct {
-	boardImage *ebiten.Image
+	tileImage *ebiten.Image
+	board     *Board
+}
+
+func NewGame() (*Game, error) {
+	// todo: there should be a NewBoard
+	board := &Board{
+		Coordinate{1, 1},
+		2,
+		2,
+	}
+	game := &Game{
+		tileImage: nil,
+		board:     board,
+	}
+	return game, nil
 }
 
 func (g *Game) Update() error {
@@ -18,23 +33,18 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	tileSize := 20
 	tileOffset := tileSize + 2
 
-	g.boardImage = ebiten.NewImage(tileSize, tileSize)
+	// todo: there should be a board.draw where the tileImage is passed
+	g.tileImage = ebiten.NewImage(tileSize, tileSize)
 	screen.Fill(color.Color(color.RGBA{0xCC, 0xCC, 0xCC, 0xFF}))
-	g.boardImage.Fill(color.Color(color.RGBA{0x00, 0x00, 0x00, 0xFF}))
+	g.tileImage.Fill(color.Color(color.RGBA{0x00, 0x00, 0x00, 0xFF}))
 
-	screen.DrawImage(g.boardImage, nil)
-
-	options := &ebiten.DrawImageOptions{}
-	options.GeoM.Translate(float64(tileOffset), float64(0))
-	screen.DrawImage(g.boardImage, options)
-
-	options = &ebiten.DrawImageOptions{}
-	options.GeoM.Translate(float64(0), float64(tileOffset))
-	screen.DrawImage(g.boardImage, options)
-
-	options = &ebiten.DrawImageOptions{}
-	options.GeoM.Translate(float64(tileOffset), float64(tileOffset))
-	screen.DrawImage(g.boardImage, options)
+	for row := 0; row < g.board.height; row++ {
+		for column := 0; column < g.board.width; column++ {
+			options := &ebiten.DrawImageOptions{}
+			options.GeoM.Translate(float64(column*tileOffset), float64(row*tileOffset))
+			screen.DrawImage(g.tileImage, options)
+		}
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -44,7 +54,8 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	game, err := NewGame()
+	if err = ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
