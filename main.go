dccc4bff8a6b97a1b599c9714c09f5d051940242
Stefan Boos <kontakt@boos.systems>
2024-09-30 17:47:55 +0200

    feat: we can display a 2x2 board (ugly)
    Co-authored-by: Stefan Boos <kontakt@boos.systems>
    Co-authored-by: Niklas Rueber <niklas@rueber.cloud>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>

--- main.go
+++ main.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
+	"image/color"
 	"log"
-
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+	boardImage *ebiten.Image
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	//ebitenutil.DebugPrint(screen, "Hello, World!")
+	g.boardImage = ebiten.NewImage(20, 20)
+	screen.Fill(color.Color(color.RGBA{0xCC, 0xCC, 0xCC, 0xFF}))
+	g.boardImage.Fill(color.Color(color.RGBA{0x00, 0x00, 0x00, 0xFF}))
+
+	screen.DrawImage(g.boardImage, nil)
+
+	options := &ebiten.DrawImageOptions{}
+	options.GeoM.Translate(float64(22), float64(0))
+	screen.DrawImage(g.boardImage, options)
+
+	options = &ebiten.DrawImageOptions{}
+	options.GeoM.Translate(float64(0), float64(22))
+	screen.DrawImage(g.boardImage, options)
+
+	options = &ebiten.DrawImageOptions{}
+	options.GeoM.Translate(float64(22), float64(22))
+	screen.DrawImage(g.boardImage, options)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
