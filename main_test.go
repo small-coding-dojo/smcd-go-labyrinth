694d62061df1097c1d4a34270550fd8c73231048
Knesch <bernhardschenk@gmx.de>
2024-10-15 17:33:32 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:main_test.go

--- main_test.go
+++ main_test.go
@@ -45,7 +45,14 @@ func TestBoardHeight(t *testing.T) {
 // Given a window size of 640 x 480
 // When a 2 x 2 board is requested
 // Then the tile size should be 480 / 2 = 240
-func Test_(t *testing.T) {
-	// got := calculateTileSize(640, 480)
-	// if got != 240 {}
+func TestTileSizeFor640x480Board(t *testing.T) {
+	got := calculateTileSize(640, 480)
+	want := 240
+	if got != want {
+		t.Errorf("got %q, but want %q", got, want)
+	}
+}
+
+func calculateTileSize(boardWidth, boardHeight int) int {
+	return 240 // Has to be calculated by width and height
 }
